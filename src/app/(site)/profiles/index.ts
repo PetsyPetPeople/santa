40708001d3fa1d446c1eae315d2cc97980c0c9b7
Tuan <75029513+tuanna7494@users.noreply.github.com>
2023-10-30 14:00:03 +0700

    Update node.js.yml

--- src/app/(site)/profiles/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './components';
-export * from './movieSlice';
-export * from './services';
-export * from './types';
-export * from './hooks';
--- src/app/(site)/profiles/types.ts
+++
@@ -1,9 +0,0 @@
-import { ELoading } from '@/constants';
-import { Movie } from '@/libs/graphql';
-
-export interface IMovieState {
-  entities: Movie[];
-  loading: ELoading;
-  currentRequestId: string | undefined;
-  error: string | null;
-}
